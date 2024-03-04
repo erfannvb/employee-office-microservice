32971f9d2725ebfdce4069ea4adb3250b36e3e31
erfannvb <erfannavab@gmail.com>
2024-03-04 23:15:50 +0330

    created the employee entity

---
+++ employee-service/src/main/java/nvb/dev/employeeservice/dao/entity/EmployeeEntity.java
@@ -0,0 +1,23 @@
+package nvb.dev.employeeservice.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document("employee")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EmployeeEntity {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+
+}
