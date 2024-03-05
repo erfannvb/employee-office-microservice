c88ce458aba54ea0a1c492edd7de9c44cdfd8b8b
erfannvb <erfannavab@gmail.com>
2024-03-05 20:43:17 +0330

    created global exception handler

---
+++ office-service/src/main/java/nvb/dev/officeservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package nvb.dev.officeservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(OfficeNotFoundException.class)
+    public ResponseEntity<Object> handleOfficeNotFoundException(RuntimeException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(exception.getLocalizedMessage());
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timeStamp", errorResponse.getTimeStamp());
+        body.put("message", errorResponse.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+}
