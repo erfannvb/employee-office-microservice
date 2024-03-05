366dd54df1ea8057bc9fffacd14a14397482fcf6
erfannvb <erfannavab@gmail.com>
2024-03-05 10:05:55 +0330

    added @Builder

--- employee-service/src/main/java/nvb/dev/employeeservice/dao/dto/EmployeeRequest.java
+++ employee-service/src/main/java/nvb/dev/employeeservice/dao/dto/EmployeeRequest.java
@@ -1,10 +1,12 @@
 package nvb.dev.employeeservice.dao.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class EmployeeRequest {
 
     private String firstName;
