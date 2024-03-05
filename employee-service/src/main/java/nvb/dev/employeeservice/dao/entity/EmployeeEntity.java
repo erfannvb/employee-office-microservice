b8873d17e5de9e18ab8589f4689e46bb3840f3ac
erfannvb <erfannavab@gmail.com>
2024-03-05 09:54:54 +0330

    - added @Builder - made the class an entity - added generated value to the id

--- employee-service/src/main/java/nvb/dev/employeeservice/dao/entity/EmployeeEntity.java
+++ employee-service/src/main/java/nvb/dev/employeeservice/dao/entity/EmployeeEntity.java
@@ -1,21 +1,24 @@
 package nvb.dev.employeeservice.dao.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
-@Document(value = "employee")
+@Entity
+@Table(name = "tbl_employees")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class EmployeeEntity {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
