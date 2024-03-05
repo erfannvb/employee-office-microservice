7e3afc02870639e6e0adbe80648670e0a1ead3aa
erfannvb <erfannavab@gmail.com>
2024-03-05 21:30:09 +0330

    enabled the discovery client

--- office-service/src/main/java/nvb/dev/officeservice/OfficeServiceApplication.java
+++ office-service/src/main/java/nvb/dev/officeservice/OfficeServiceApplication.java
@@ -5,12 +5,14 @@ import nvb.dev.officeservice.repository.OfficeRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 import java.util.UUID;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OfficeServiceApplication {
 
     public static void main(String[] args) {
