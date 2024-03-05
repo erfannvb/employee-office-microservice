8885aa18880c7b6d6365ba0034a839b556622d0b
erfannvb <erfannavab@gmail.com>
2024-03-05 09:55:26 +0330

    - added @Builder - changed id type to Long

--- employee-service/src/main/java/nvb/dev/employeeservice/dao/dto/EmployeeResponse.java
+++ employee-service/src/main/java/nvb/dev/employeeservice/dao/dto/EmployeeResponse.java
@@ -1,13 +1,15 @@
 package nvb.dev.employeeservice.dao.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class EmployeeResponse {
 
-    private String id;
+    private Long id;
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
