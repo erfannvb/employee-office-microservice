95f47284609dcbf6077281952965f4a5c402598b
erfannvb <erfannavab@gmail.com>
2024-03-05 10:24:19 +0330

    created office-service module

---
+++ office-service/src/test/java/nvb/dev/officeservice/OfficeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nvb.dev.officeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
