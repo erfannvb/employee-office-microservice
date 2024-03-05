d52d522c5c2b0eaf6d01d046534e2517e1487bea
erfannvb <erfannavab@gmail.com>
2024-03-05 09:53:43 +0330

    extended JpaRepository and used Long for the id

--- employee-service/src/main/java/nvb/dev/employeeservice/repository/EmployeeRepository.java
+++ employee-service/src/main/java/nvb/dev/employeeservice/repository/EmployeeRepository.java
@@ -1,9 +1,9 @@
 package nvb.dev.employeeservice.repository;
 
 import nvb.dev.employeeservice.dao.entity.EmployeeEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends MongoRepository<EmployeeEntity, String> {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 }
