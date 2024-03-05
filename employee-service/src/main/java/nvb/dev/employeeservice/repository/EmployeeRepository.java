45afc7ac9dd023c4a90444a5acf7761cc8748d18
erfannvb <erfannavab@gmail.com>
2024-03-05 13:15:06 +0330

    added findByOfficeName method

--- employee-service/src/main/java/nvb/dev/employeeservice/repository/EmployeeRepository.java
+++ employee-service/src/main/java/nvb/dev/employeeservice/repository/EmployeeRepository.java
@@ -4,6 +4,11 @@ import nvb.dev.employeeservice.dao.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+
+    Optional<EmployeeEntity> findByOfficeName(String officeName);
+
 }
