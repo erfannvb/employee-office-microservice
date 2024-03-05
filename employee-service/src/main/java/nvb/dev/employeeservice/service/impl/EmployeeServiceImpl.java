e943c01650f5bee9e8128e466e8b04ffb837bb82
erfannvb <erfannavab@gmail.com>
2024-03-05 20:50:51 +0330

    fixed the method

--- employee-service/src/main/java/nvb/dev/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/nvb/dev/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -3,27 +3,50 @@ package nvb.dev.employeeservice.service.impl;
 import lombok.RequiredArgsConstructor;
 import nvb.dev.employeeservice.dao.dto.EmployeeRequest;
 import nvb.dev.employeeservice.dao.dto.EmployeeResponse;
+import nvb.dev.employeeservice.dao.dto.OfficeResponse;
 import nvb.dev.employeeservice.dao.entity.EmployeeEntity;
+import nvb.dev.employeeservice.exception.EmployeeNotFoundException;
 import nvb.dev.employeeservice.repository.EmployeeRepository;
 import nvb.dev.employeeservice.service.EmployeeService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final WebClient.Builder webClientBuilder;
 
     @Override
     public EmployeeEntity createEmployee(EmployeeRequest employeeRequest) {
-        return employeeRepository.save(EmployeeEntity.builder()
-                .firstName(employeeRequest.getFirstName())
-                .lastName(employeeRequest.getLastName())
-                .dateOfBirth(employeeRequest.getDateOfBirth())
-                .officeName(employeeRequest.getOfficeName())
-                .build());
+        EmployeeEntity employee = new EmployeeEntity();
+
+        employee.setFirstName(employeeRequest.getFirstName());
+        employee.setLastName(employeeRequest.getLastName());
+        employee.setDateOfBirth(employeeRequest.getDateOfBirth());
+        employee.setOfficeName(employeeRequest.getOfficeName());
+
+        // Call office-service, add employee if the office exists
+        OfficeResponse response = webClientBuilder.build().get()
+                .uri("http://localhost:8383/api/v1/office",
+                        uriBuilder -> uriBuilder.queryParam("officeName", employee.getOfficeName()).build())
+                .retrieve()
+                .bodyToMono(OfficeResponse.class)
+                .block();
+
+        assert response != null;
+        boolean officeNameExists = response.isExists();
+
+        if (officeNameExists) {
+            return employeeRepository.save(employee);
+        } else {
+            throw new EmployeeNotFoundException(employeeRequest.getOfficeName());
+        }
     }
 
     @Override
