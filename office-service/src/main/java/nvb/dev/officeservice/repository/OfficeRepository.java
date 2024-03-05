5d2880f212cdf0c3e9a4ebd7f996e93d2f986982
erfannvb <erfannavab@gmail.com>
2024-03-05 16:09:11 +0330

    added findByOfficeName method

--- office-service/src/main/java/nvb/dev/officeservice/repository/OfficeRepository.java
+++ office-service/src/main/java/nvb/dev/officeservice/repository/OfficeRepository.java
@@ -4,6 +4,11 @@ import nvb.dev.officeservice.dao.entity.OfficeEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OfficeRepository extends MongoRepository<OfficeEntity, String> {
+
+    Optional<OfficeEntity> findByOfficeName(String officeName);
+
 }
