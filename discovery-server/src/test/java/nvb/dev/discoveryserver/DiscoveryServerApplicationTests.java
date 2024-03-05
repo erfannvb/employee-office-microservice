fdef5829c26aec63aa007aca670248eee39d9b47
erfannvb <erfannavab@gmail.com>
2024-03-05 21:08:19 +0330

    created discovery-server module

---
+++ discovery-server/src/test/java/nvb/dev/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nvb.dev.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
