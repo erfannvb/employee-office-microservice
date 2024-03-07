ad375f0683946bd0a4b40962041f93625dfac5d9
erfannvb <erfannavab@gmail.com>
2024-03-07 08:23:02 +0330

    - changed the return type to CompletableFuture  for using time limiter - added @TimeLimiter to the createEmployee method - used const var

--- employee-service/src/main/java/nvb/dev/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/nvb/dev/employeeservice/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package nvb.dev.employeeservice.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import nvb.dev.employeeservice.dao.dto.EmployeeRequest;
 import nvb.dev.employeeservice.dao.dto.EmployeeResponse;
@@ -11,22 +12,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping(path = "/api/v1")
 @RequiredArgsConstructor
 public class EmployeeController {
 
+    private static final String OFFICE_SERVICE = "officeService";
+
     private final EmployeeService employeeService;
 
     @PostMapping(path = "/employee")
-    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    public ResponseEntity<EmployeeEntity> createEmployee(@RequestBody EmployeeRequest employeeRequest) {
-        return new ResponseEntity<>(employeeService.createEmployee(employeeRequest), HttpStatus.CREATED);
+    @CircuitBreaker(name = OFFICE_SERVICE, fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = OFFICE_SERVICE)
+    public CompletableFuture<ResponseEntity<EmployeeEntity>> createEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        return CompletableFuture.supplyAsync(() -> new ResponseEntity<>(
+                employeeService.createEmployee(employeeRequest),
+                HttpStatus.CREATED));
     }
 
-    public ResponseEntity<EmployeeEntity> fallbackMethod(Exception exception) {
-        return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+    public CompletableFuture<ResponseEntity<EmployeeEntity>> fallbackMethod(Exception exception) {
+        return CompletableFuture.supplyAsync(() -> new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE));
     }
 
     @GetMapping(path = "/employee/all")
