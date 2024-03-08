4397ea6d5d8ff4a6788e596c1c96e2b078866c0e
erfannvb <erfannavab@gmail.com>
2024-03-08 22:01:59 +0330

    added a method to handle kafka notification

--- notification-service/src/main/java/nvb/dev/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/nvb/dev/notificationservice/NotificationServiceApplication.java
@@ -1,13 +1,22 @@
 package nvb.dev.notificationservice;
 
+import lombok.extern.slf4j.Slf4j;
+import nvb.dev.notificationservice.event.CreateEmployeeEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@Slf4j
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @KafkaListener(topics = "notificationTopic")
+    public void handleNotification(CreateEmployeeEvent event) {
+        log.info("Received Notification for Employee - {}", event.getFirstName() + " " + event.getLastName());
     }
 
 }
