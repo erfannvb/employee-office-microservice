1efcda041ecdbd195b07fd5c52152b57fe2e7343
erfannvb <erfannavab@gmail.com>
2024-03-09 08:44:07 +0330

    enabled discovery client

--- notification-service/src/main/java/nvb/dev/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/nvb/dev/notificationservice/NotificationServiceApplication.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import nvb.dev.notificationservice.event.CreateEmployeeEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Slf4j
 public class NotificationServiceApplication {
 
