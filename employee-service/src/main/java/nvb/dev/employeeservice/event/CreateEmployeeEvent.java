2a6734a119a382bc24199de5f795428d4b9e6b27
erfannvb <erfannavab@gmail.com>
2024-03-09 11:50:10 +0330

    added @NoArgsConstructor

--- employee-service/src/main/java/nvb/dev/employeeservice/event/CreateEmployeeEvent.java
+++ employee-service/src/main/java/nvb/dev/employeeservice/event/CreateEmployeeEvent.java
@@ -2,8 +2,10 @@ package nvb.dev.employeeservice.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CreateEmployeeEvent {
     private String firstName;
--- notification-service/src/main/java/nvb/dev/notificationservice/event/CreateEmployeeEvent.java
+++ notification-service/src/main/java/nvb/dev/notificationservice/event/CreateEmployeeEvent.java
@@ -2,8 +2,10 @@ package nvb.dev.notificationservice.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CreateEmployeeEvent {
     private String firstName;
